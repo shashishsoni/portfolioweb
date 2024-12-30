78195afe9dacaf65793705ec19385db2d0b5fb22
shashishsoni <shshshsoni2003@tutanota.com>
2024-12-31 02:54:19 +0530

    code 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavPage from './components/NavPage';
+
+const App = () => {
+  return (
+    <>
+    <NavPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
+const NavPage = () => {
+  return (
+    <div>NavPage</div>
+  )
+}
+
+export default NavPage
